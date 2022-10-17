c2e52fa173706c6977d44b3f9171251ce3a29812
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2022-10-17 13:27:10 -0400

    continuação do projeto Quiz, alteração

--- script.js
+++ script.js
@@ -3,16 +3,39 @@ let currentQuestion = 0;
 showQuestion();
 
 function showQuestion() {
-  if (questions[currentQuestion]) {
+  if(questions[currentQuestion]) {
     let q = questions[currentQuestion];
 
-    document.querySelector(".scoreArea").getElementsByClassName.display =
-      "none";
-    document.querySelector(".questionArea").getElementsByClassName.display =
-      "block";
+   document.querySelector('.scoreArea').style.display = 'none';
+   document.querySelector('.questionArea').style.display = 'block';
 
-    document.querySelector(".question").innerHTML = q.question;
+   document.querySelector('.question').innerHTML = q.question;
+
+
+   let optionsHtml = '';
+   for(let i in q.options) {
+   optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`
+   }
+
+   document.querySelector('.options').innerHTML = optionsHtml;
+
+   document.querySelectorAll('.options .option').forEach(item => {
+    item.addEventListener('click', optionClickEvent);
+   })
+
   } else {
     //
   }
+}
+
+function optionClickEvent(e) {
+  let clickedOption = parseInt(e.target.getAttribute('data-op'));
+
+  if(questions[currentQuestion].answer === clickedOption) {
+    console.log("Acertou!")
+
+  } else {
+    console.log("Errou!")
+  }
+
 }
