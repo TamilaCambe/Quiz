7b151bff7bc6898837cace7ea3860159705435d9
Tamila Cambé <98542594+TamilaCambe@users.noreply.github.com>
2022-11-02 17:26:53 -0300

    Changing the questions
    Changing the questions

--- questions.js
+++ questions.js
@@ -1,103 +1,96 @@
-let questions = [
-  {
-    question: "Em qual elemento HTML nós colocamos o javascript?",
-    options: ["tag js", "tag script", "tag scripting", "tag javascript"],
-    answer: 1,
-  },
-  {
-    question: "Onde é o local certo de colocar o javascript?",
-    options: [
-      "Na tag body",
-      "Na tag head",
-      "Tanto na tag body quanto a head estão corretas",
-    ],
-    answer: 2,
-  },
-  {
-    question:
-      "Qual a sintaxe correta para adicionar um arquivo javascript externo na tag script?",
-    options: ['name="xxx.js"', 'src="xxx.js"', 'href="xxx.js"'],
-    answer: 1,
-  },
-  {
-    question: "Um arquivo javascript externo precisa ter a tag script",
-    options: ["Verdade", "Falso"],
-    answer: 1,
-  },
-  {
-    question: 'Como escrever um "Olá Mundo" em um alertbox?',
-    options: [
-      'alertBox("Olá Mundo")',
-      'msgBox("Olá Mundo")',
-      'msg("Olá Mundo")',
-      'alert("Olá Mundo")',
-    ],
-    answer: 3,
-  },
-  {
-    question: "Como você cria uma função no Javascript?",
-    options: [
-      "function:minhaFuncao()",
-      "function = minhaFuncao()",
-      "function minhaFuncao()",
-    ],
-    answer: 2,
-  },
-  {
-    question: 'Como chamar uma função chamada "minhaFuncao"?',
-    options: ["call minhaFuncao", "call function minhaFuncao", "minhaFuncao()"],
-    answer: 2,
-  },
-  {
-    question: "Como escrever uma condicional IF no Javascript?",
-    options: ["if i = 5", "if i == 5 then", "if (i == 5)", "if i = 5 then"],
-    answer: 2,
-  },
-  {
-    question:
-      'Como fazer um if que executa um código caso "i" for diferente de 5',
-    options: ["if (i != 5)", "if (i <> 5)", "if i <> 5", "if i =! 5 then"],
-    answer: 0,
-  },
-  {
-    question: "Como o loop while começa?",
-    options: ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10"],
-    answer: 0,
-  },
-
-  {
-    question: "Como o loop while começa?",
-    options: ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10"],
-    answer: 0,
-  },
-
-  {
-    question: "Como o loop while começa?",
-    options: ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10"],
-    answer: 0,
-  },
-
-  {
-    question: "Como o loop while começa?",
-    options: ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10"],
-    answer: 0,
-  },
-
-  {
-    question: "Como o loop while começa?",
-    options: ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10"],
-    answer: 0,
-  },
-
-  {
-    question: "Como o loop while começa?",
-    options: ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10"],
-    answer: 0,
-  },
-
-  {
-    question: "Como o loop while começa?",
-    options: ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10"],
-    answer: 0,
-  },
+let questions = [{
+        question: 'Qual a diferença entre == E ===',
+        options: [
+            '== compara valor e tipo E === compara o valor',
+            'é uma escolha pessoal, tanto faz usar == ou === no código', //OK
+            '== compara o valor E === compara o valor e o tipo',
+            '== atribui valor à variável E === compara valor e tipo'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual a diferenca entre loops while e do-while em JavaScript?',
+        options: [
+            'While: executa o loop primeiro e depois verifica a condição. Do While: executa o loop enquanto a condição for verdadeira',
+            'While: executa o loop enquanto a condição for verdadeira. Do While: executa o loop primeiro e depois verifica a condição',
+            'While: Executa o loop sobre os elementos de um array. Do While: Executa o loop enquanto a condição for verdadeira.'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual das definições de função abaixo NÃO faz parte de JavaScript?',
+        options: [
+            'Function declaration',
+            'Arrow function',
+            'Function expression',
+            'Public function'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Na programação assíncrona as funções são executadas em ordem.',
+        options: [
+            'Verdade',
+            'Falso'
+        ],
+        answer: 1
+    },
+    {
+        question: 'O que significa Cookies?',
+        options: [
+            'Permitem que os aplicativos armazenem dados no navegador de um cliente.',
+            'Permitem que os aplicativos armazenem dados até que a janela do navegador seja fechada.',
+            'Permitem que os aplicativos armazenem dados sem uma data de expiração.'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual método adiciona um ou mais elementos no fim de um array e retorna o comprimento resultante do array',
+        options: [
+            'pop()',
+            'slice()',
+            'push()',
+            'unshift()'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Como escrever uma condicional IF no Javascript?',
+        options: [
+            'if i = 5',
+            'if i == 5 then',
+            'if (i == 5)',
+            'if i = 5 then'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual método abaixo permite iterar sobre o array e modificar seus elementos usando uma função de callback?',
+        options: [
+            'forEach()',
+            'filter()',
+            'reduce()',
+            'map()'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Qual a diferença entre: function person() {}, const person = function() {}, const person = () => {}',
+        options: [
+            'Function expression, Function declaration, Arrow function',
+            'Function declaration, Function expression, Arrow function',
+            'Function declaration, Function expression, Arrow constructor',
+            'Function expression, Function constructor, Arrow function'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Como o loop for começa?',
+        options: [
+            'for (let i = 0; i < array.length; i++)',
+            'for (let i < 0; i = array.length; i++)',
+            'for (let i = 0; i = array.length; i++)'
+        ],
+        answer: 0
+    },
 ];
